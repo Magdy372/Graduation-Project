014f13ce2d2dbbf3afdd62a3892dab93606a41b1
nadaelamir101 <nada2103057@miuegypt.edu.eg>
2025-05-02 19:10:18 +0300

    imgs

--- graduation-front/src/components/Banner.jsx
+++ graduation-front/src/components/Banner.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import backgroundPic from "./../assets/images/map1.jpg";
 
 // Import additional images
-import BannerPng from "../assets/images/banner.webp";
+import BannerPng from "../assets/images/family.webp";
 import ExtraImg1 from "../assets/images/img1.jpg";
 import ExtraImg2 from "../assets/images/img2.jpg";
 
